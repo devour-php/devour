a65029398724cbc91a40123927674a99b1861878
Chris Leppanen <chris.leppanen@gmail.com>
2014-01-06 23:19:58 -0500

    Doc strings.

--- src/Devour/Row/SimplePieRow.php
+++ src/Devour/Row/SimplePieRow.php
@@ -7,6 +7,9 @@
 
 namespace Devour\Row;
 
+/**
+ * @todo Add all fields.
+ */
 class SimplePieRow implements RowInterface {
 
   protected $title;
@@ -21,10 +24,23 @@ class SimplePieRow implements RowInterface {
 
   protected $author_name;
 
+  /**
+   * Sets a value.
+   *
+   * @param string $field
+   *   The field to set.
+   * @param mixed $value
+   *   The value.
+   */
   public function set($field, $value) {
     $this->$field = $value;
   }
 
+  /**
+   * Translates a field value.
+   *
+   * @todo Implement.
+   */
   protected function translate($target_field) {
     return $target_field;
   }
