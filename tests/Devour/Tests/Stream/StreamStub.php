f1226b835e01693378d403b0a8241a22453306d5
Chris Leppanen <chris.leppanen@gmail.com>
2014-01-09 02:59:26 -0500

    Enhance StreamStub.

--- tests/Devour/Tests/Stream/StreamStub.php
+++ tests/Devour/Tests/Stream/StreamStub.php
@@ -17,10 +17,18 @@ class StreamStub extends Stream {
   /**
    * Constructs a StreamStub object.
    */
-  public function __construct($file = NULL) {
+  public function __construct($file = NULL, $raw = FALSE) {
+    if ($file && $raw) {
+      $handle = fopen('php://temp', 'rw+');
+      fwrite($handle, $file);
+
+      return $this->setStream($handle, strlen($file));
+    }
+
     if (!$file) {
       $file = 'php://temp';
     }
+
     $this->setStream(fopen($file, 'r+'), NULL);
   }
 
