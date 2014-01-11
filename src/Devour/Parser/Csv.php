79631ac16f92f255a09ffbd2da1556c8ed42fd31
Chris Leppanen <chris.leppanen@gmail.com>
2014-01-10 21:04:32 -0500

    Flesh out more of the Csv parser.

--- src/Devour/Parser/Csv.php
+++ src/Devour/Parser/Csv.php
@@ -15,31 +15,76 @@ use Guzzle\Stream\StreamInterface;
 
 /**
  * A CSV parser.
+ *
+ * @todo Handle encoding.
  */
 class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
 
   use HasTableFactoryTrait;
 
+  /**
+   * The length of the longest line in the file.
+   *
+   * Setting this can be a performance improvement. Zero means to limit.
+   *
+   * @var int
+   */
   protected $length = 0;
 
+  /**
+   * The field delimiter (one character only).
+   *
+   * @var string
+   */
   protected $delimiter = ',';
 
+  /**
+   * The field enclosure (one character only).
+   *
+   * @var string
+   */
   protected $enclosure = '"';
 
+  /**
+   * The escape character (one character only).
+   *
+   * @var string
+   */
   protected $escape = '\\';
 
-  protected $pointer = 0;
-
+  /**
+   * Whether the file contains a header.
+   *
+   * @var bool
+   */
   protected $hasHeader = FALSE;
 
+  /**
+   * The number of lines to read in one batch.
+   *
+   * @var int
+   */
   protected $limit = 50;
 
+  /**
+   * The number of lines that have been read during the current batch.
+   *
+   * @var int
+   */
   protected $linesRead;
 
+  /**
+   * What fgetcsv() considers an empty line.
+   *
+   * We skip empty lines. Store it so that we aren't creating a bunch of empty
+   * arrays.
+   *
+   * @var array
+   */
   protected $emptyLine;
 
   /**
-   * Constructs a new Csv object.
+   * Constructs a Csv object.
    */
   public function __construct() {
     $this->emptyLine = array(NULL);
@@ -49,18 +94,22 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
    * {@inheritdoc}
    */
   public static function fromConfiguration(array $configuration) {
+    // If we wrap this array, PHPUnit says we didn't cover the last line.
+    // @todo Figure out why.
+    $configuration += array('has_header' => FALSE, 'length' => 0, 'delimiter' => ',', 'enclosure' => '"', 'escape' => '\\');
+
     $parser = new static();
-    if (!empty($configuration['has_header'])) {
-      $parser->setHasHeader(TRUE);
-    }
+    $parser->setHasHeader((bool) $configuration['has_header'])
+           ->setLineLength((int) $configuration['length'])
+           ->setDelimiter($configuration['delimiter'])
+           ->setEnclosure($configuration['enclosure'])
+           ->setEscape($configuration['escape']);
 
     return $parser;
   }
 
   /**
    * {@inheritdoc}
-   *
-   * @todo Handle encoding.
    */
   public function parse(SourceInterface $source, StreamInterface $stream) {
     $state = $source->getState($this);
@@ -72,10 +121,11 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
     // Initial load.
     if ($state->isFirstRun()) {
       $state->fileLength = $stream->getSize();
-    }
 
-    if ($this->hasHeader && !isset($state->header)) {
-      $state->header = $this->readLine($handle);
+
+      if ($this->hasHeader) {
+        $state->header = $this->readLine($handle);
+      }
     }
 
     // Reset our counter.
@@ -98,34 +148,6 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
   }
 
   /**
-   * Sets whether or not this CSV file contains a header row.
-   *
-   * @param bool $has_header
-   *   True if the file contains a header, false if not.
-   *
-   * @return self
-   *   The parser object for chaining.
-   */
-  public function setHasHeader($has_header) {
-    $this->hasHeader = $has_header;
-    return $this;
-  }
-
-  /**
-   * Sets the number of lines to parse at one time.
-   *
-   * @param int $limit
-   *   The number of lines to parse.
-   *
-   * @return self
-   *   The parser object for chaining.
-   */
-  public function setProcessLimit($limit) {
-    $this->limit = $limit;
-    return $this;
-  }
-
-  /**
    * Reads a CSV line from a handle.
    *
    * This increments the counter.
@@ -133,8 +155,8 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
    * @param resource $handle
    *   A file handle.
    *
-   * @return array|false
-   *   An indexed array containing the fields read or FALSE on other errors.
+   * @return array|bool
+   *   An indexed array containing the fields read or false on error.
    *
    * @see self::readLine()
    */
@@ -155,8 +177,8 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
    * @param resource $handle
    *   An open file handle.
    *
-   * @return array|false
-   *   An indexed array containing the fields read or FALSE on other errors.
+   * @return array|bool
+   *   An indexed array containing the fields read or false on error.
    */
   protected function readLine($handle) {
 
@@ -178,6 +200,94 @@ class Csv implements ParserInterface, ProgressInterface, ConfigurableInterface {
     }
 
     return ProgressInterface::COMPLETE;
+  }
+
+  /**
+   * Sets whether or not this CSV file contains a header row.
+   *
+   * @param bool $has_header
+   *   True if the file contains a header, false if not.
+   *
+   * @return self
+   *   The parser object for chaining.
+   */
+  public function setHasHeader($has_header) {
+    $this->hasHeader = $has_header;
+    return $this;
+  }
+
+  /**
+   * Sets the length of the longest line in the file.
+   *
+   * Setting this can be a performance improvement. Zero means to limit.
+   *
+   * @param int $length
+   *   The length of the longest line.
+   *
+   * @return self
+   *   The parser object for chaining.
+   *
+   * @see fgetcsv()
+   */
+  public function setLineLength($length) {
+    $this->length = $length;
+    return $this;
+  }
+
+  /**
+   * Sets the delimiter character (one character only).
+   *
+   * @param string $delimiter
+   *   The delimiter character.
+   *
+   * @return self
+   *   The parser object for chaining.
+   *
+   * @see fgetcsv()
+   */
+  public function setDelimiter($delimiter) {
+    $this->delimiter = $delimiter;
+    return $this;
+  }
+
+  /**
+   * Sets the enclosure character (one character only).
+   *
+   * @param string $enclosure
+   *   The enclosure character.
+   *
+   * @return self
+   *   The parser object for chaining.
+   *
+   * @see fgetcsv()
+   */
+  public function setEnclosure($enclosure) {
+    $this->enclosure = $enclosure;
+    return $this;
+  }
+
+  /**
+   * Sets the escape character (one character only).
+   *
+   * @param string $escape
+   *   The escape character.
+   *
+   * @return self
+   *   The parser object for chaining.
+   *
+   * @see fgetcsv()
+   */
+  public function setEscape($escape) {
+    $this->escape = $escape;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setProcessLimit($limit) {
+    $this->limit = $limit;
+    return $this;
   }
 
 }
