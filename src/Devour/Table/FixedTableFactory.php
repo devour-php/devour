506b76a3386bf90fd57b610c5e5bed9942d995ef
Chris Leppanen <chris.leppanen@gmail.com>
2014-01-13 01:08:36 -0500

    Document TableFactoryInterface.

--- src/Devour/Table/FixedTableFactory.php
+++ src/Devour/Table/FixedTableFactory.php
@@ -6,13 +6,32 @@ use Devour\Map\MapInterface;
 
 class FixedTableFactory implements TableFactoryInterface {
 
+  /**
+   * The table object to reuse.
+   *
+   * @var \Devour\Table\TableInterface
+   */
+  protected $table;
+
+  /**
+   * Constructs a FixedTableFactory object.
+   *
+   * @param \Devour\Table\TableInterface $table
+   *   The table to reuse.
+   */
   public function __construct(TableInterface $table) {
     $this->table = $table;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function setTableClass($class) {
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function create() {
     return $this->table;
   }
--- src/Devour/Table/TableFactoryInterface.php
+++ src/Devour/Table/TableFactoryInterface.php
@@ -1,11 +1,31 @@
 <?php
 
+/**
+ * @file
+ * Contains \Devour\Table\TableFactoryInterface.
+ */
+
 namespace Devour\Table;
 
+/**
+ * The interface for table factories.
+ */
 interface TableFactoryInterface {
 
+  /**
+   * Sets the table class.
+   *
+   * @param string $class
+   *   The table class to create.
+   */
   public function setTableClass($class);
 
+  /**
+   * Creates a new table class.
+   *
+   * @return \Devour\Table\TableInterface
+   *   A new table.
+   */
   public function create();
 
 }
