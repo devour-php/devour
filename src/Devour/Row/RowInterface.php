61ef3db634c7f3baa1deebf6cab16049b19a7a06
Chris Leppanen <chris.leppanen@gmail.com>
2014-01-13 05:10:57 -0500

    Document RowInterface.

--- src/Devour/Row/RowInterface.php
+++ src/Devour/Row/RowInterface.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \Devour\Row\RowInterface
+ * Contains \Devour\Row\RowInterface.
  */
 
 namespace Devour\Row;
@@ -13,26 +13,56 @@ use Devour\Table\TableInterface;
  * The interface for a single row in a table.
  */
 interface RowInterface {
+
   /**
+   * Constructs a RowInterface object.
    *
+   * @param \Devour\Table\TableInterface $table
+   *   The table the row belongs to.
    */
   public function __construct(TableInterface $table);
 
   /**
    * Returns the value for a target field.
    *
-   * @param string $target_field
-   *   The name of the target field.
+   * @param string $field
+   *   The name of the field.
    *
-   * @return mixed
-   *   The value that corresponds to this field.
+   * @return mixed|null
+   *   The value that corresponds to this field, or null if it does not exist.
    */
-  public function get($target_field);
+  public function get($field);
 
-  public function set($source_field, $value);
+  /**
+   * Sets a value for a field.
+   *
+   * @param string $field
+   *   The name of the field.
+   * @param mixed $value
+   *   The value for the field.
+   *
+   * @return self
+   *   The row for chaining.
+   */
+  public function set($field, $value);
 
+  /**
+   * Returns the entire internal data array.
+   *
+   * @return array
+   *   The internal data array.
+   */
   public function getData();
 
+  /**
+   * Sets the data array.
+   *
+   * @param array $data
+   *   The array to use for the row data.
+   *
+   * @return self
+   *   The row for method chaining.
+   */
   public function setData(array $data);
 
 }
