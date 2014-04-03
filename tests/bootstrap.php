803dd53f7c5915542e65473dcd18069695097e30
Chris Leppanen <chris.leppanen@gmail.com>
2014-04-03 11:35:28 -0700

    Don't include TestRunner manually.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,6 @@
 
 error_reporting(-1);
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 define('DEVOUR_COMMAND_START_DIR', dirname(__DIR__) . '/bin');
